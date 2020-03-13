8a8bec73a674b6343abd806d2c11a8ee72648011
amt145 <60409801+amt145@users.noreply.github.com>
2020-03-13 01:44:02 -0400

    mplab x project updated

--- RouterCardSecondary.X/Init.c
+++ RouterCardSecondary.X/Init.c
@@ -1,7 +1,6 @@
 #include "Init.h"
 #include "timers.h"
 #include "uart1_config.h"
-//#include "uart2_config.h"
 #include "mcc_generated_files/system.h"
 #include "mcc_generated_files/can1.h"
 #include "commsReceive.h"
@@ -10,9 +9,8 @@ void init(void) {
     SYSTEM_Initialize();
     timer1_init();
     uart1_init();
-    TRISBbits.TRISB10 = 0;
-    //uart2_init();
-    //initCOMs();
+//    TRISBbits.TRISB10 = 0;
+    initCOMs();
     CAN1_TransmitEnable();
     CAN1_ReceiveEnable();
 }
